9cffc02894d2e244f2af8dca909474ad0ed4d742
Rizki <mhdrizky32@gmail.com>
2024-03-13 05:21:47 +0700

    Adding: Some Logic To Filter

--- public/scripts/app.example.js
+++ public/scripts/app.example.js
@@ -4,18 +4,17 @@ class App {
     this.loadButton = document.getElementById("load-btn");
     this.carContainerElement = document.getElementById("cars-container");
     this.tipeDriver = document.getElementById("tipeDriver");
-    this.tanggal = document.getElementById("tanggal");
-    this.waktuJemput = document.getElementById("waktuJemput");
+    this.tanggalSewa = document.getElementById("tanggalSewa");
+    this.waktuSewa = document.getElementById("waktuSewa");
     this.jumlahPenumpang = document.getElementById("jumlahPenumpang");
+
+    // Button
+    this.loadButton = document.getElementById("load-btn");
   }
 
   async init() {
     await this.load();
     this.run();
-
-    // Register click listener
-    // this.clearButton.onclick = this.clear;
-    // this.loadButton.onclick = this.run;
   }
 
   run = () => {
@@ -33,18 +32,59 @@ class App {
   async load() {
     const cars = await Binar.listCars();
     Car.init(cars);
-    console.log(cars);
+  }
+
+  async loadfilter() {
+    const cars = await Binar.listCars((data) => {
+      const tanggalJemputData = new Date(data.availableAt).getTime();
+      const tanggal = new Date(
+        `${this.tanggal.value} ${this.waktuJemput.value}`
+      ).getTime();
+      const checkWaktu = tanggalJemputData >= tanggal;
+      const availableAt =
+        this.tipeDriver.value === "true" && data.available ? true : false;
+      const notAvailableAt =
+        this.tipeDriver.value === "false" && !data.available ? true : false;
+      const penumpang = data.capacity >= this.jumlahPenumpang.value;
+      if (
+        this.tipeDriver.value !== "default" &&
+        this.tanggal.value !== "" &&
+        this.waktuJemput.value !== "false" &&
+        this.jumlahPenumpang.value >= 0
+      ) {
+        return (availableAt || notAvailableAt) && checkWaktu && penumpang;
+      } else if (
+        this.tipeDriver.value !== "default" &&
+        this.jumlahPenumpang.value > 0
+      ) {
+        return (availableAt || notAvailableAt) && penumpang;
+      } else if (
+        this.tanggal.value !== "" &&
+        this.waktuJemput.value !== "false" &&
+        this.jumlahPenumpang.value > 0
+      ) {
+        return checkWaktu && penumpang;
+      } else if (
+        this.tanggal.value !== "" &&
+        this.waktuJemput.value !== "false"
+      ) {
+        return checkWaktu;
+      } else if (this.tipeDriver.value !== "default") {
+        return availableAt || notAvailableAt;
+      } else {
+        return penumpang;
+      }
+    });
+    console.log("makan" + { cars });
+    Car.init(cars);
   }
 
   clear = () => {
-    // let child = this.carContainerElement.firstElementChild;
+    let child = this.carContainerElement.firstElementChild;
 
-    // while (child) {
-    //   child.remove();
-    //   child = this.carContainerElement.firstElementChild;
-    // }
-
-    // Membersihkan konten cars-container
-    this.carContainerElement.innerHTML = "";
+    while (child) {
+      child.remove();
+      child = this.carContainerElement.firstElementChild;
+    }
   };
 }
